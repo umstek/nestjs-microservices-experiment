ac95268d4a8adbbc7d49add870e76e1f8be7c47f
Wickramaranga Abeygunawardhana <umstek@live.com>
2020-01-25 20:05:45 +0530

    Change gRPC port
    .. okay, found it

--- src/grpc-client.options.ts
+++ src/grpc-client.options.ts
@@ -5,6 +5,7 @@ export const grpcClientOptions: ClientOptions = {
   transport: Transport.GRPC,
   options: {
     package: 'hero',
-    protoPath: join(__dirname, './hero/hero.proto')
+    protoPath: join(__dirname, './hero/hero.proto'),
+    url: '127.0.0.1:50051'
   }
 };
